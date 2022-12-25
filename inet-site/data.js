265d3dbe48a7c77af20550016fda4a0c3ff8fc2b
Andrii Yudov <andrii.yudov@pitchbook.com>
2022-12-25 12:55:39 +0200

    Create i-net site

---
+++ inet-site/data.js
@@ -0,0 +1,52 @@
+const DATA = [
+    {
+        name: 'Phones',
+        products: [
+            {
+                name: 'Apple iPhone',
+                price: 1000,
+                description: 'Some short description'
+            },
+            {
+                name: 'Samsung S#',
+                price: 2000,
+                description: 'Some very long description. Some very long description. Some very long description. Some very long description'
+            },
+            {
+                name: 'Google Pixel',
+                price: 3000,
+                description: 'Some common description. Some common description.'
+            }
+        ]
+    },
+    {
+        name: 'Laptops',
+        products: [
+            {
+                name: 'ThinkPad',
+                price: 1000,
+                description: 'Some short description'
+            },
+            {
+                name: 'Macbook Pro',
+                price: 2000,
+                description: 'Some very long description. Some very long description. Some very long description. Some very long description'
+            }
+        ]
+    },
+    {
+        name: 'Tablets',
+        products: [
+            {
+                name: 'Samsung Tab',
+                price: 1000,
+                description: 'Some short description'
+            },
+            {
+                name: 'Apple iPad',
+                price: 2000,
+                description: 'Some very long description. Some very long description. Some very long description. Some very long description'
+            }
+        ]
+    }
+];
