be5a9a2a2b8e2480062527976da29e6bfc24a344
Andrii Yudov <andrii.yudov@pitchbook.com>
2023-02-25 19:46:58 +0200

    Small updates in `webpack` example

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
             directory: path.join(__dirname, 'dist')
         },
         compress: true,
-        port: 3000,
+        port: 3001,
     },
     module: {
         rules: [
