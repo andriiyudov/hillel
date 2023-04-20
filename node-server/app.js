4040d684bd05813c7599f0830c56fd608a0af871
Andrii Yudov <andrii.yudov@pitchbook.com>
2023-04-20 18:23:50 +0300

    Rework `node-server` example to work with contacts and messages

--- node-server/app.js
+++ node-server/app.js
@@ -1,46 +1,46 @@
 import express from 'express';
 import cors from 'cors';
 
-import {USERS} from './data.js';
+import {CONTACTS, MESSAGES} from './data.js';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-server.get('/users', (req, res) => {
-    res.send(USERS);
+server.get('/contacts', (req, res) => {
+    res.send(CONTACTS);
 });
 
-server.post('/users', (req, res) => {
-    const user = req.body;
-    console.log(req.body)
-    USERS.push(user);
+server.post('/contacts', (req, res) => {
+    const contact = req.body;
+    CONTACTS.push(contact);
+
+    res.sendStatus(200);
+});
+
+server.get('/messages', (req, res) => {
+    res.send(MESSAGES)
+});
+
+server.post('/messages', (req, res) => {
+    const {message, conversationId} = req.body;
+    const prevMessages = MESSAGES[conversationId] || [];
+
+    MESSAGES[conversationId] = [...prevMessages, message];
+
+    res.sendStatus(200);
+});
+
+server.post('/messages/delete', (req, res) => {
+    const {id, conversationId} = req.body;
+    const prevMessages = MESSAGES[conversationId];
+    MESSAGES[conversationId] = prevMessages.filter(message => message.id !== id);
+
     res.sendStatus(200);
 });
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
-
-
-// const http = require('http');
-//
-//
-// const server = http.createServer((request, response) => {
-//     if (request.url === '/') {
-//         response.statusCode = 200;
-//         response.setHeader('Content-Type', 'text/plain');
-//         response.end('Hello World\n');
-//     } else if (request.url === '/users') {
-//         response.statusCode = 200;
-//         response.setHeader('Content-Type', 'application/json');
-//         response.end(JSON.stringify([{user: 'nanm'}]))
-//     }
-//     console.log(request.url)
-// });
-//
-// server.listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-// });
--- node-server/data.js
+++ node-server/data.js
@@ -1,1 +1,5 @@
-export const USERS = [{name: 'Andrew', age: 25, card: '1234567773543', email: 'sfds@sf.com'}];
+export const CONTACTS = [{name: 'Andrew', id: '-1'}];
+
+export const MESSAGES = {
+    '-1': [{text: 'Hello from server', id: '-1', isCurrentUser: true}]
+};
